39b1ee6836d333b33b311a291fe32e1f08e7ae46
Terje Kern <terje.kern@icloud.com>
2025-12-02 11:12:58 +0100

    refactor: remove unnecessary React imports from App and VideoCard components; streamline code structure

--- video-spotify-feed/src/App.tsx
+++ video-spotify-feed/src/App.tsx
@@ -1,24 +1,13 @@
-import React from "react";
 import "./App.css";
 import Navbar from "./Navbar";
-import { BrowserRouter, Route, Routes } from "react-router";
+import PostFeed from "./PostFeed";
 
 function App() {
   return (
-    <>
-      <div>
-        <Navbar />
-        const root = document.getElementById("root");
-        ReactDOM.createRoot(root).render(
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<App />} />
-            <Route path="/" element={<App />} />
-          </Routes>
-        </BrowserRouter>
-        , );
-      </div>
-    </>
+    <div>
+      <Navbar />
+      <PostFeed />
+    </div>
   );
 }
 
--- video-spotify-feed/src/PostFeed.tsx
+++ video-spotify-feed/src/PostFeed.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import fallbackVideos from "./data/videos";
 import VideoCard from "./components/VideoCard";
 
