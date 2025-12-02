917b83e59c8f4b3b19f5f92a9d88b1eb30f6340a
Nick Steiner <nick.steiner@bbz-sh.ch>
2025-12-02 11:33:34 +0100

    feat: add Login component and update Navbar for navigation to Login page

--- video-spotify-feed/src/App.tsx
+++ video-spotify-feed/src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Login from "./Login";
 import Navbar from "./Navbar";
 import PostFeed from "./PostFeed";
 import { Routes, Route } from "react-router";
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<PostFeed />} />
+        <Route path="Login" element={<Login />} />
       </Routes>
     </div>
   );
