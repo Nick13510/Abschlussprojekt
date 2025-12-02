316c14eee7563d1683c3a51849f520cf219b48c6
Terje Kern <terje.kern@icloud.com>
2025-12-02 10:49:52 +0100

    fix: clean up App component structure and ensure proper rendering of Navbar and PostFeed

--- video-spotify-feed/src/App.tsx
+++ video-spotify-feed/src/App.tsx
@@ -2,26 +2,24 @@ import React from "react";
 import "./App.css";
 import Navbar from "./Navbar";
 import PostFeed from "./PostFeed";
+import { BrowserRouter, Route, Routes } from "react-router";
 
 function App() {
   return (
     <div>
-      <Navbar />
-<<<<<<< HEAD
-      const root = document.getElementById("root");
-      ReactDOM.createRoot(root).render(
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/" element={<App />} />
-        </Routes>
-      </BrowserRouter>
-      , );
-    </>
-=======
-      <PostFeed />
+      <>
+        <Navbar />
+        const root = document.getElementById("root");
+        ReactDOM.createRoot(root).render(
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/" element={<App />} />
+          </Routes>
+        </BrowserRouter>
+        , );
+      </>
     </div>
->>>>>>> d0b5f1ff547d45a5a32c5dd5dd4a2353c679a6b2
   );
 }
 
