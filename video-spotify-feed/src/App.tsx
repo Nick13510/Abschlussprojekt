35d6bfbaf3707d472664ca1557e6a7227970259f
Terje Kern <terje.kern@icloud.com>
2025-12-02 11:38:18 +0100

    feat: add RegisterPage route to App component for user registration

--- video-spotify-feed/src/App.tsx
+++ video-spotify-feed/src/App.tsx
@@ -2,13 +2,14 @@ import "./App.css";
 import Navbar from "./Navbar";
 import PostFeed from "./PostFeed";
 import { Routes, Route } from "react-router";
-
+import RegisterPage from "./Register";
 function App() {
   return (
     <div>
       <Navbar />
       <Routes>
         <Route path="/" element={<PostFeed />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </div>
   );
