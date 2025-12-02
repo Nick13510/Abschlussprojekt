16222ae67f4ac814d57935631b48a4f4618b9b4f
Nick Steiner <nick.steiner@bbz-sh.ch>
2025-12-02 11:17:39 +0100

    feat: integrate routing in App component; set up BrowserRouter and Routes for navigation

--- video-spotify-feed/src/App.tsx
+++ video-spotify-feed/src/App.tsx
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
 import "./App.css";
 import Navbar from "./Navbar";
 import PostFeed from "./PostFeed";
@@ -7,6 +10,14 @@ function App() {
     <div>
       <Navbar />
       <PostFeed />
+      const root = document.getElementById("root");
+      ReactDOM.createRoot(root).render(
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+      , );
     </div>
   );
 }
